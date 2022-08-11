22bfb12adc516643e1bd14a22182b857b07cf37c
symphony <jciurc@gmail.com>
2022-08-10 18:31:06 -0700

    update app component and add snowpack component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import React from "react";
+import Snowpack from "./components/Snowpack";
+
 const App = () => {
   return (
+    <div>
     <h2>hello ❄️Snowpack❄️</h2>
+    <Snowpack />
+    </div>
   );
 };
 
--- src/components/Snowpack.tsx
+++ src/components/Snowpack.tsx
@@ -3,11 +3,13 @@ import React, {useState, useEffect} from 'react';
 function Snowpack() {
   // Create the count state.
   const [count, setCount] = useState(0);
+
   // Update the count (+1 every second).
   useEffect(() => {
     const timer = setTimeout(() => setCount(count + 1), 1000);
     return () => clearTimeout(timer);
   }, [count, setCount]);
+
   // Return the App component.
   return (
     <div className="Snowpack">
